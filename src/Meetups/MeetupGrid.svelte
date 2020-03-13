e68c38918894010665a025511411f720117e5271
3daddict <print@3daddict.com>
2020-03-13 15:39:18 -0700

    done

---
+++ src/Meetups/MeetupGrid.svelte
@@ -0,0 +1,34 @@
+<script>
+    import MeetupItem from './MeetupItem.svelte';
+
+    export let meetups;
+</script>
+
+<style>
+section {
+  width: 100%;
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-gap: 1rem;
+}
+
+@media (min-width: 768px) {
+  section {
+    grid-template-columns: repeat(2, 1fr);
+  }
+}
+
+</style>
+
+<section id="meetups">
+{#each meetups as meetup}
+    <MeetupItem 
+        title = {meetup.title}
+        subtitle = {meetup.subtitle}
+        imageUrl = {meetup.imageUrl}
+        description = {meetup.description}
+        email = {meetup.contactEmail}
+        address = {meetup.address}
+    />
+{/each}
+</section>
